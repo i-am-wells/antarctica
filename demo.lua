ae3671c5e83e93547a375c9ab50681ede9844dc7
iwells2 <iwells2@illinois.edu>
2017-11-27 15:20:27 -0600

    added a few more comments

--- demo.lua
+++ demo.lua
@@ -1,4 +1,4 @@
-
+-- include antarctica things
 local ant = require 'antarctica'
 
 local Tilemap = require 'tilemap'
@@ -6,24 +6,28 @@ local Image = require 'image'
 local Object = require 'object'
 local Engine = require 'engine'
 
-
+-- the viewport
 local vx, vy, vw, vh
 vx = -200
 vy = -150
 vw = 400
 vh = 300
 
-local engine = Engine{ title = 'Demo', w = 800, h = 600, 
+-- Create a window. "setlogicalsize" lets us render everything normally but
+-- will scale the canvas to match the window.
+local engine = Engine{ title = 'Demo', w = vw * 2, h = vh * 2, 
         windowflags = 0 }
 engine:setlogicalsize(vw, vh)
-engine:setcolor(255, 255, 255, 255)
+engine:setcolor(255, 255, 255, 255) -- white background
 
+-- Load the map
 local map = Tilemap{ file = 'test.map' }
 
+-- Load the tiles and sprites
 local image = Image{ engine = engine, file = 'res/terrain.png', tw = 16, th = 16 }
 
-
-local guy = Object{
+-- Create the player object. It will use sprites from terrain.png.
+local penguin = Object{
     image = image,
     x = 0,
     y = 0,
@@ -37,65 +41,75 @@ local guy = Object{
 }
 
 
-guy.wx = 0
-guy.wy = 0
-guy.stepsize = 2
+-- These will store penguin's walking velocity.
+penguin.wx = 0
+penguin.wy = 0
+penguin.stepsize = 2
 
-guy.walkinfo = {
+-- Used for looking up which sprite to use for each direction the penguin
+-- can face.
+penguin.walkinfo = {
     north = { tx = 11, ty = 0 },
     south = { tx = 10, ty = 0 },
     east = { tx = 12, ty = 0 },
     west = { tx = 13, ty = 0 }
 }
-guy.walkY = { 0, 1, 0, 2 }
+-- For the walking animation
+penguin.walkY = { 0, 1, 0, 2 }
 
 
-function guy:turn(direction)
-    guy.direction = direction
-    local winfo = guy.walkinfo[direction]
-    guy:setsprite(winfo.tx, winfo.ty)
+-- Turns the penguin to face north, south, east, or west
+function penguin:turn(direction)
+    penguin.direction = direction
+    local winfo = penguin.walkinfo[direction]
+    penguin:setsprite(winfo.tx, winfo.ty)
 end
 
-function guy:setspriteY(count)
+-- Update the penguin's sprite to create the walking animation
+function penguin:setspriteY(count)
     local frame = (count // 4) % 4
-    local winfo = guy.walkinfo[guy.direction]
-    guy:setsprite(winfo.tx, winfo.ty + guy.walkY[frame+1])
+    local winfo = penguin.walkinfo[penguin.direction]
+    penguin:setsprite(winfo.tx, winfo.ty + penguin.walkY[frame+1])
 end
 
--- convert between map and screen coordinates
+-- convert between map and pixel coordinates
 local screentomap = function(x, y)
     local mapx = (x) // image.tw
     local mapy = (y) // image.th
     return mapx, mapy
 end
 
-function guy:checkwallbump()
-    if guy.x <= 0 and guy.wx < 0 then
-        guy.wx = 0
+
+-- Stop the penguin if it reaches a wall
+function penguin:checkwallbump()
+    if penguin.x <= 0 and penguin.wx < 0 then
+        penguin.wx = 0
     end
 
-    if guy.y <= 0 and guy.wy < 0 then
-        guy.wy = 0
+    if penguin.y <= 0 and penguin.wy < 0 then
+        penguin.wy = 0
     end
 
-    local mapx, mapy = screentomap(guy.x, guy.y)
+    local mapx, mapy = screentomap(penguin.x, penguin.y)
     if (map:get_flags(0, mapx, mapy) & ant.tilemap.bumpeastflag) ~= 0 then
-        if guy.wx < 0 then guy.wx = 0 end
+        if penguin.wx < 0 then penguin.wx = 0 end
     end
     if (map:get_flags(0, mapx + 1, mapy) & ant.tilemap.bumpwestflag) ~= 0 then
-        if guy.wx > 0 then guy.wx = 0 end
+        if penguin.wx > 0 then penguin.wx = 0 end
     end
     if (map:get_flags(0, mapx, mapy) & ant.tilemap.bumpsouthflag) ~= 0 then
-        if guy.wy < 0 then guy.wy = 0 end
+        if penguin.wy < 0 then penguin.wy = 0 end
     end
     if (map:get_flags(0, mapx, mapy + 1) & ant.tilemap.bumpnorthflag) ~= 0 then
-        if guy.wy > 0 then guy.wy = 0 end
+        if penguin.wy > 0 then penguin.wy = 0 end
     end
 end
 
 
-map:addObject(guy)
+map:addObject(penguin)
 
+
+-- Add NPCs
 local dummylocations = {
     {x = 100, y = 100},
 }
@@ -119,22 +133,23 @@ end
 
 
 
+-- Keyboard handling callbacks
 onkeydown = setmetatable({
         D = function() 
-            guy:turn('east')
-            guy.wx = guy.stepsize 
+            penguin:turn('east')
+            penguin.wx = penguin.stepsize 
         end,
         W = function() 
-            guy:turn('north')
-            guy.wy = -guy.stepsize 
+            penguin:turn('north')
+            penguin.wy = -penguin.stepsize 
         end,
         A = function() 
-            guy:turn('west')
-            guy.wx = -guy.stepsize 
+            penguin:turn('west')
+            penguin.wx = -penguin.stepsize 
         end,
         S = function() 
-            guy:turn('south')
-            guy.wy = guy.stepsize 
+            penguin:turn('south')
+            penguin.wy = penguin.stepsize 
         end,
         Escape = function() print('quitting...') engine:stop() end
     },
@@ -144,10 +159,10 @@ onkeydown = setmetatable({
 )
 
 onkeyup = setmetatable({
-        D = function() guy.wx = 0 guy:turn(guy.direction) end,
-        W = function() guy.wy = 0 guy:turn(guy.direction) end,
-        A = function() guy.wx = 0 guy:turn(guy.direction) end,
-        S = function() guy.wy = 0 guy:turn(guy.direction) end,
+        D = function() penguin.wx = 0 penguin:turn(penguin.direction) end,
+        W = function() penguin.wy = 0 penguin:turn(penguin.direction) end,
+        A = function() penguin.wx = 0 penguin:turn(penguin.direction) end,
+        S = function() penguin.wy = 0 penguin:turn(penguin.direction) end,
     },
     {
         __index = function() return function() end end
@@ -159,6 +174,8 @@ local min = function(a, b)
     if a > b then return a else return b end
 end
 
+
+
 engine:run{
     redraw = function(time, elapsed, counter)
         local actual_vx = min(0, vx)
@@ -169,14 +186,14 @@ engine:run{
         map:draw_layer_objects(0, actual_vx, actual_vy, vw, vh)
 
         -- Update position
-        guy:checkwallbump()
-        guy:move(guy.wx, guy.wy)
-        vx = vx + guy.wx
-        vy = vy + guy.wy
+        penguin:checkwallbump()
+        penguin:move(penguin.wx, penguin.wy)
+        vx = vx + penguin.wx
+        vy = vy + penguin.wy
 
         -- Update sprite
-        if guy.wx ~= 0 or guy.wy ~= 0 then
-            guy:setspriteY(counter)
+        if penguin.wx ~= 0 or penguin.wy ~= 0 then
+            penguin:setspriteY(counter)
         end
     end,
 
