68d94a4228a633b8cf17b03e898e8e10ed0f822a
Ian Wells <iwells018@gmail.com>
2020-04-22 20:37:14 -0700

    make it easier to use gdb with antarctica.sh

--- antarctica.sh
+++ antarctica.sh
@@ -4,7 +4,7 @@ ANT_DEBUG=${ANT_DEBUG:-false}
 
 ROOT=$(dirname "$(realpath "$0")")
 
-lua -e "__dbg, __rootdir = $ANT_DEBUG, '$ROOT'" \
+$WRAPPER lua -e "__dbg, __rootdir = $ANT_DEBUG, '$ROOT'" \
   -e "package.path=package.path..';$ROOT/lua/?.lua;$ROOT/?.lua'" \
   -e "package.cpath=package.cpath..';$ROOT/?.so'" \
   -l antarctica \
